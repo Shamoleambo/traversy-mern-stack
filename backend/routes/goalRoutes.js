1ba801389c4dd9cf28262b52cab8430b5d05621f
Tiago Gomes <tidgomes@gmail.com>
2022-12-01 12:08:21 -0300

    add post put and delete routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Get goals' })
+  res.status(200).json({ message: 'Get goals' })
+})
+
+router.post('/', (req, res) => {
+  res.status(201).json({ message: 'Set goals' })
+})
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` })
 })
 
 module.exports = router
