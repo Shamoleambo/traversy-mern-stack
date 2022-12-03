62252d650433eadbdfc4f5540a68a75b5dbe3ef2
Tiago Gomes <tidgomes@gmail.com>
2022-12-03 11:16:55 -0300

    set delete goal functionality for goalsController

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -22,18 +22,27 @@ const setGoal = asyncHandler(async (req, res) => {
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id)
 
-  if(!goal){
+  if (!goal) {
     res.status(400)
     throw new Error('Goal not found')
   }
 
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
 
   res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('No goal found to be deleted')
+  }
+
+  await goal.remove()
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal }
