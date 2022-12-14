a835ef734e42b87a7e8898f8d61feee35f55ddac
Tiago Gomes <tidgomes@gmail.com>
2022-12-14 10:38:02 -0300

    create Login page form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,63 @@
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+
 function Login() {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({ email: '', password: '' })
+
+  const { email, password } = formData
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              className='form-control'
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              className='form-control'
+              placeholder='Enter your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default Login
