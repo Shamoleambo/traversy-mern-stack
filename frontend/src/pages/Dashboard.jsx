382d611d04cb0ce1b05083a1f3aff1836b9cc6cf
Tiago Gomes <tidgomes@gmail.com>
2023-01-02 18:05:22 -0300

    fetch the goals on the Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,36 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import { getGoals, reset } from '../features/goals/goalSlice'
+import Spinner from '../components/Spinner'
 
 function Dashboard() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const { user } = useSelector(state => state.auth)
+  const { goals, isLoading, isError, message } = useSelector(
+    state => state.goals
+  )
 
   useEffect(() => {
+    if (isError) console.log(message)
+
     if (!user) navigate('/login')
-  }, [user, navigate])
+
+    dispatch(getGoals())
+
+    /*
+      Commented out because this was looping, and the following warning was being displayed in the browser's console:
+        -->Throttling history state changes to prevent the browser from hanging<--
+    */
+    // return () => {
+    //   dispatch(reset())
+    // }
+  }, [user, isError, message, navigate, dispatch])
+
+  if (isLoading) return <Spinner />
 
   return (
     <>
