b62f47cf1f420008baf29a40571c5097c35b6853
Tiago Gomes <tidgomes@gmail.com>
2023-01-02 11:40:09 -0300

    create login functions for authSlice and authService

--- frontend/src/features/authService.js
+++ frontend/src/features/authService.js
@@ -2,12 +2,20 @@ import axios from 'axios'
 
 const API_URL = 'api/users/'
 
-const register = async (userData) => {
+const register = async userData => {
   const response = await axios.post(API_URL, userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
   }
+
+  return response.data
+}
+
+const login = async userData => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if (response.data) localStorage.setItem('user', JSON.stringify(response.data))
 
   return response.data
 }
@@ -18,7 +26,8 @@ const logout = async () => {
 
 const authService = {
   register,
-  logout
+  logout,
+  login
 }
 
 export default authService
